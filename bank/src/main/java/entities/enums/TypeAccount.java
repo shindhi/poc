379d50e6f719695ab1a343c6c474d720855d1888
Diogo Naka <diogo.naka@invillia.com>
2019-10-16 10:14:33 -0300

    Remove Map from TypeAccount and add .gitignore file

--- bank/src/main/java/entities/enums/TypeAccount.java
+++ bank/src/main/java/entities/enums/TypeAccount.java
@@ -7,15 +7,6 @@ public enum TypeAccount {
     CHECKIG_ACCOUNT("CHECKING", 500.0),
     SAVINGS_ACCOUNT("SAVING", 0.0);
 
-    private static final Map<String, TypeAccount> BY_TYPE = new HashMap<String, TypeAccount>();
-    private static final Map<Double, TypeAccount> BY_LIMIT = new HashMap<Double, TypeAccount>();
-
-    static {
-        for (TypeAccount a : values()) {
-            BY_TYPE.put(a.typeAccount, a);
-            BY_LIMIT.put(a.loanLimit, a);
-        }
-    }
 
     public final String typeAccount;
     public final Double loanLimit;
